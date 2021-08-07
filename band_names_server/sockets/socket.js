8c89735df4eed2d47e8912d40a3447a879fb85a9
michael rondon <mrondon72@gmail.com>
2021-08-07 20:39:55 +0200

    34. Socket: Emitir bandas registradas

--- band_names_server/sockets/socket.js
+++ band_names_server/sockets/socket.js
@@ -1,8 +1,19 @@
 const { io } = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+bands.addBand( new Band('Queen') );
+bands.addBand( new Band('Bon Jovi') );
+bands.addBand( new Band('Héroes del Silencio') );
+bands.addBand( new Band('Metallica') );
 
 // Mensajes de Sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
+
+    client.emit('active-bands', bands.getBands() );
 
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
