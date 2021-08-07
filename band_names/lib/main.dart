e79cc46233281a37309e5fec05975b60795103aa
michael rondon <mrondon72@gmail.com>
2021-08-07 14:48:16 +0200

    8. Inicio de proyecto - BandNamesApp

---
+++ band_names/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+ 
+import 'package:band_names/src/pages/home.dart';
+
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'home',
+      routes: {
+        'home': (_) => HomePage()
+      },
+      title: 'Material App'
+    );
+  }
+}
