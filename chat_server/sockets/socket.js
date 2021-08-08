2385e6e2515b6fa9d7e26e1e049c24f71b887a74
michael rondon <mrondon72@gmail.com>
2021-08-08 15:42:38 +0200

    73. Inicio de sesion - ChatApp Backend

---
+++ chat_server/sockets/socket.js
@@ -0,0 +1,10 @@
+const { io } = require('../index');
+
+// Mensajes de Sockets
+io.on('connection', client => {
+    console.log('Cliente conectado');
+
+    client.on('disconnect', () => {
+        console.log('Cliente desconectado');
+    });
+});
