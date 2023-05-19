8849a95842540d4b7ef96ac623fb249d8df2dc58
Michael Rondon <michael.rondon@ezzing.com>
2023-05-19 13:13:39 +0200

    8. Inicio de proyecto - BandNamesApp

--- band_names/lib/main.dart
+++ band_names/lib/main.dart
@@ -1,28 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
- 
-import 'package:band_names/pages/home.dart';
-import 'package:band_names/pages/status.dart';
-import 'package:band_names/services/socket_service.dart';
 
-void main() => runApp(MyApp());
- 
+import 'package:band_names/screens/home.dart';
+
+void main() => runApp(const MyApp());
+
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        initialRoute: 'home',
-        routes: {
-          'home': (_) => HomePage(),
-          'status': (_) => StatusPage(),
-        },
-        title: 'Material App'
-      ),
-      providers: [
-        ChangeNotifierProvider(create: (_) => SocketService())
-      ],
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'home',
+      routes: {
+        'home': (_) => const HomeScreen()
+      },
+      title: 'Material App'
     );
   }
 }
