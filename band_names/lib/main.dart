044d5a6f906f8a50c1521c26891cbedc681d4ff7
Michael Rondon <michael.rondon@ezzing.com>
2023-05-19 13:38:44 +0200

    10. Interfaz basica de nuestro HomePage

--- band_names/lib/main.dart
+++ band_names/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+/// Screens
 import 'package:band_names/screens/home.dart';
 
 void main() => runApp(const MyApp());
--- band_names/lib/screens/home.dart
+++ band_names/lib/screens/home.dart
@@ -1,15 +1,55 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-   
+/// Models
+import 'package:band_names/models/band.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
-  
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<Band> bands = [
+    Band(id: '1', name: 'Metallica', votes: 5),
+    Band(id: '2', name: 'Queen', votes: 1),
+    Band(id: '3', name: 'Héroes del Silencio', votes: 2),
+    Band(id: '4', name: 'Bon Jovi', votes: 5),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('HomeScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        elevation: 1,
+        title: const Text('BandNames', style: TextStyle(color: Colors.black87))
       ),
+      body: ListView.builder(
+        itemBuilder: (BuildContext context, int i) => _bandTile(bands[i]),
+        itemCount: bands.length
+      ),
+      floatingActionButton: FloatingActionButton(
+        elevation: 1,
+        onPressed: () {},
+        child: const Icon(Icons.add)
+      ),
+    );
+  }
+
+  ListTile _bandTile(Band band) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundColor: Colors.blue[100],
+        child: Text(band.name.substring(0, 2)),
+      ),
+      onTap: () {
+        print(band.name);
+      },
+      title: Text(band.name),
+      trailing: Text('${band.votes}', style: const TextStyle(fontSize: 20)),
     );
   }
 }
